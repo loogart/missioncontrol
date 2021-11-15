0f491e586f8182f977c072b1ac8bc34a5b1252ae
Chris Soueidan <chris@loogart.com>
2021-11-14 19:02:26 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {    
+    extend: {
+      colors:{
+        primary: '#2A96B8',
+      },
+      fontFamily:{
+        'sans': ['"Nunito"', 'sans-serif'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
